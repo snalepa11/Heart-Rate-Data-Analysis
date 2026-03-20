07b003a7d06051a4fe4a2ef23aecf23c8d8a78c3
Snalepa <snalepa1998@gmail.com>
2026-03-19 22:01:51 -0400

    Implemented run function

--- main.py
+++ main.py
@@ -60,56 +60,52 @@ def range(data: list) -> float:
     return my_range 
 
 
-file_object = open("/Users/sarahnalepa/Desktop/TKH/Labs/hr_data_pipeline/Heart-Rate-Data-Analysis/data/phase0.txt")
-readData = file_object.readlines()
-clean_data = clean_heartrate_data(readData)
-data_average = average(clean_data)
-data_median = median(clean_data)
-print(range(clean_data))
-
-# def rolling_avg(data: list, k: int) -> float:
-#     """
-#     CHALLENGE FUNCTION (Optional)
-#     """
-#     pass
 
 
-# def run(file: str):
-#     """
-#     Process heart rate data from the a file by cleaning and
-#     calculating summary statistics. Print out final values.
 
-#     Args:
-#         filename (str): The path to the data file (e.g., 'data/phase0.txt').
+def rolling_avg(data: list, k: int) -> float:
+    """
+    CHALLENGE FUNCTION (Optional)
+    """
+    pass
 
-#     Returns:
-#         float, float, float: You will return the average, median, and range.
-#     """
-#     data = []
+
+def run(file: str):
+    """
+    Process heart rate data from the a file by cleaning and
+    calculating summary statistics. Print out final values.
+
+    Args:
+        filename (str): The path to the data file (e.g., 'data/phase0.txt').
+
+    Returns:
+        float, float, float: You will return the average, median, and range.
+    """
 
 #     # open file using file I/O and read it into the `data` list
-    
-#     data = file_object.readlines()
-#     #strip white space including /n .strip()
-#     removed_values = data.strip(/n)
-#     #Cast string values to numeric types with int() and float()
-    
-#     # Use `clean_heartrate_data` to clean the data and remove invalid entries
-#     cleaned_list, removed_values = clean_heartrate_data(data)
+    file_object = open(file)
+    readData = file_object.readlines()
 
 #     # calculate the average, median, and range of this file using the functions you've wrote
 #     average(data)
 #     median(data)
 #     range(data)
+    clean_data = clean_heartrate_data(readData)
+    data_average = average(clean_data)
+    data_median = median(clean_data)
+    data_range = range(clean_data)
+    print(range(clean_data))
 
 #     # print out your data quality measure to the console
-#     print(data)
+    print(clean_data)
 #     # print out your descriptive statistics to the console
 #     print(average, median, range)
+    print(data_range)
+    print(data_median)
+    print(data_average)
 
-
-# if __name__ == "__main__":
-#     run("data/phase0.txt")
-#     run("data/phase1.txt")
-#     run("data/phase2.txt")
-#     run("data/phase3.txt")
+if __name__ == "__main__":
+    run("data/phase0.txt")
+    run("data/phase1.txt")
+    run("data/phase2.txt")
+    run("data/phase3.txt")
